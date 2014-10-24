94f65cff2dd53943c9eceb23de042e517c6d3956
Wang Zhipeng <ddmad1030@gmail.com>
2014-10-24 22:05:42 +0800

    Create an add function for user model.

--- model/user.js
+++ model/user.js
@@ -30,4 +30,35 @@ User.get = function(pid, callback) {
   });
 };
 
+User.prototype.add = function(name, passport, email, password, contact, callback) {
+	var md5 = crypto.createHash('md5'),
+		password_MD5 = md5.update(password).digest('hex');
+		console.log(password_MD5);
+
+	var user = {
+		name = this.name;
+		passport = this.passport;
+		email = this.email;
+		password = this. password_MD5;
+		contact = this.contact;
+	};
+
+	pool.getConection(function(err, connection) {
+		connection.query('INSERT INTO passenger SET ?', user, function(err, rows, fields) {
+			if (err) {
+				return callback(err.code, null);
+			}
+
+			var output = [];
+			for (var i in rows) {
+				output.push(rows[i]);
+			}
+			callback(null, output);
+
+			connection.release();
+		});
+	});
+
+};
+
 module.exports = User;
